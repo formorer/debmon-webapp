5aa19b14ae765a5ad0b5dfa53437ae943b34e7da
Alexander Wirt <formorer@debian.org>
2012-10-09 07:51:42 +0200

    Add instructions site

--- lib/DebMonWeb.pm
+++ lib/DebMonWeb.pm
@@ -13,6 +13,7 @@ sub startup {
   my $config = $self->plugin('Config', {file  => $configfile});
   # Normal route to controller
   $r->get('/')->to('static#home')->name('home');
+  $r->get('/instructions')->to('static#instructions')->name('instructions');
   $r->get('/packages')->to('packages#overview')->name('packageoverview');
   $r->get('/packages/:dist/:package')->to('packages#package')->name('package');
 }
